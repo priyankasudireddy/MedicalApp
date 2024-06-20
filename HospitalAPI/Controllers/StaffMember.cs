c6c479a32527f5b1310da4550e009924054de4e4
trainingforgit <ankitharikkala0421@gmail.com>
2024-06-20 15:37:40 -0500

    Changes Done

--- HospitalAPI/Controllers/StaffMember.cs
+++ HospitalAPI/Controllers/StaffMember.cs
@@ -2,5 +2,9 @@
 {
     public class StaffMember
     {
+        public int Ward { get; set; }
+        public int Room { get; set; }
+        public String Equipment { get; set; }
+        public int Staff { get; set; }
     }
 }
