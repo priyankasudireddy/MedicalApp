875543f9afb6c61dd69e465a6fa059bc4a87a37a
trainingforgit <ankitharikkala0421@gmail.com>
2024-06-20 15:16:18 -0500

    Added Controller

---
+++ HospitalAPI/Controllers/Hospital ManagmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalAPI.Controllers
+{
+    public class Hospital_ManagmentController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
